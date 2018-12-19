1f95429f2b0ce07bb139f5f53d978081c067b393
Abhay Verma <abhay.verma@snapdeal.com>
2018-12-19 17:50:50 +0530

    code to dump and re-created entire infra in neo4j

---
+++ dumpYamlsNeo4j.py
@@ -0,0 +1,59 @@
+import glob
+import os.path
+import time
+from py2neo import Graph
+from scripts.appProps import convertYamlTojson
+from scripts import cypherNeo4j
+
+neo4j_host = "localhost"
+neo4j_http_port = 8474
+neo4j_bolt_port = 8687
+neo4j_user = ""
+neo4j_pass = ""
+
+
+def dump():
+
+    graph = cypherNeo4j.authenticate(neo4j_host,neo4j_http_port,neo4j_bolt_port,neo4j_user,neo4j_pass)
+    graph.delete_all()
+
+    comps = [os.path.basename(f).split('.')[0] for f in glob.glob("services/components/*.yml")]
+
+    error = []
+    for comp in comps:
+        if(comp != "nonengg"):
+            document = convertYamlTojson(comp)
+
+            query_subcomponent = """
+            WITH {json} AS document
+            UNWIND document.subcomponents AS subcomponent
+            MERGE (comp:Component {name: document.component})
+            MERGE (subcomp:SubComponent {name: document.component+":"+subcomponent.name})
+            """
+            #CREATE UNIQUE (comp)-[:SubComponent {name: "SubComponent"}]->(subcomp)
+
+            query_dependency = """
+            WITH {json} AS document
+            UNWIND document.subcomponents AS subcomponent
+            UNWIND subcomponent.dependencies AS dependency
+            MERGE (comp:Component {name: document.component})
+            MERGE (subcomp:SubComponent {name: document.component+":"+subcomponent.name})
+            MERGE (cd:Component {name: dependency.component})
+            MERGE (scd:SubComponent {name: dependency.component+":"+dependency.subcomponent})
+            CREATE UNIQUE (comp)-[:ConnectsTo {name: "ConnectsTo"}]->(cd)
+            CREATE UNIQUE (subcomp)-[:ConnectsTo {name: "ConnectsTo"}]->(scd)
+            """
+            #print(document)
+            try:
+                graph.run(query_subcomponent, json=document)
+                graph.run(query_dependency, json=document)
+            except Exception as err:
+                error.append(comp)                
+                print(err)
+                
+            #time.sleep(1)
+    if(len(error) > 0):
+        print("Following YAMLs Are Not Successfully Processed")
+        print(error)        
+    else:
+        print("All YAMLs Successfully Processed")
