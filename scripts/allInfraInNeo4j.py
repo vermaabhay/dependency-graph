0ba7c57963e28ad22337c0baed92ac42126ca3e4
Abhay Verma <abhay.verma@snapdeal.com>
2019-01-22 21:58:46 +0530

    moving variables out of for loop; avoiding repeated initialization

--- scripts/allInfraInNeo4j.py
+++ scripts/allInfraInNeo4j.py
@@ -13,31 +13,31 @@ def dumpAllYamlsNeo4j(neo4j_host,neo4j_http_port,neo4j_bolt_port,neo4j_user,neo4
 
     comps = [os.path.basename(f).split('.')[0] for f in glob.glob("services/components/*.yml")]
 
+    query_subcomponent = """
+        WITH {json} AS document
+        UNWIND document.subcomponents AS subcomponent
+        MERGE (comp:Component {name: document.component})
+        MERGE (subcomp:SubComponent {name: document.component+":"+subcomponent.name})
+    """
+        #CREATE UNIQUE (comp)-[:SubComponent {name: "SubComponent"}]->(subcomp)
+
+    query_dependency = """
+        WITH {json} AS document
+        UNWIND document.subcomponents AS subcomponent
+        UNWIND subcomponent.dependencies AS dependency
+        MERGE (comp:Component {name: document.component})
+        MERGE (subcomp:SubComponent {name: document.component+":"+subcomponent.name})
+        MERGE (cd:Component {name: dependency.component})
+        MERGE (scd:SubComponent {name: dependency.component+":"+dependency.subcomponent})
+        CREATE UNIQUE (comp)-[:ConnectsTo {name: "ConnectsTo"}]->(cd)
+        CREATE UNIQUE (subcomp)-[:ConnectsTo {name: "ConnectsTo"}]->(scd)
+    """
+
     error = []
     for comp in comps:
         if(comp != "nonengg"):
             document = convertYamlTojson(comp)
 
-            query_subcomponent = """
-            WITH {json} AS document
-            UNWIND document.subcomponents AS subcomponent
-            MERGE (comp:Component {name: document.component})
-            MERGE (subcomp:SubComponent {name: document.component+":"+subcomponent.name})
-            """
-            #CREATE UNIQUE (comp)-[:SubComponent {name: "SubComponent"}]->(subcomp)
-
-            query_dependency = """
-            WITH {json} AS document
-            UNWIND document.subcomponents AS subcomponent
-            UNWIND subcomponent.dependencies AS dependency
-            MERGE (comp:Component {name: document.component})
-            MERGE (subcomp:SubComponent {name: document.component+":"+subcomponent.name})
-            MERGE (cd:Component {name: dependency.component})
-            MERGE (scd:SubComponent {name: dependency.component+":"+dependency.subcomponent})
-            CREATE UNIQUE (comp)-[:ConnectsTo {name: "ConnectsTo"}]->(cd)
-            CREATE UNIQUE (subcomp)-[:ConnectsTo {name: "ConnectsTo"}]->(scd)
-            """
-            #print(document)
             try:
                 graph.run(query_subcomponent, json=document)
                 graph.run(query_dependency, json=document)
@@ -45,7 +45,6 @@ def dumpAllYamlsNeo4j(neo4j_host,neo4j_http_port,neo4j_bolt_port,neo4j_user,neo4
                 error.append(comp)                
                 print(err)
                 
-            #time.sleep(1)
     if(len(error) > 0):
         print("Following YAMLs Are Not Successfully Processed")
         print(error)        
